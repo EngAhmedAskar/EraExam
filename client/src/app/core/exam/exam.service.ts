998144b6d8396fd168ea2de80c345b21ddef3b94
Ryan <ryan.mousa14@gmail.com>
2018-10-24 10:02:38 -0500

    Merge Conflicts

--- client/src/app/core/exam/exam.service.ts
+++ client/src/app/core/exam/exam.service.ts
@@ -21,25 +21,6 @@ export class ExamService {
   constructor(private httpClient: HttpClient) {}
   getByToken(context: TokenQuestion): Observable<ListExams> {
     return this.httpClient
-<<<<<<< HEAD
-            .cache()
-            .get(routes.quote(context.token))
-            .pipe(
-              map((body: any) => body),
-              catchError(() => of('Error, could not load exam :-('))
-            );
-  }
-  getExams(): Observable<String> {
-    return this.httpClient
-            .cache()
-            .get('/exams')
-            .pipe(
-              map((body: any) => body),
-              catchError(() => of('Error, could not load Exams :-('))
-            );
-  }
-
-=======
       .cache()
       .get(routes.quote(context.token))
       .pipe(
@@ -74,5 +55,14 @@ export class ExamService {
   // getByToken(context: TokenQuestion): Promise<any> {
   //   return this.httpClient.get(routes.quote(context.token)).toPromise();
   // }
->>>>>>> 3f3afdf4ce1ccbb468f95eed69c5cbb16afc187c
+
+  getExams(): Observable<String> {
+    return this.httpClient
+      .cache()
+      .get('/exams')
+      .pipe(
+        map((body: any) => body),
+        catchError(() => of('Error, could not load Exams :-('))
+      );
+  }
 }
--- client/src/app/exam/exam.module.ts
+++ client/src/app/exam/exam.module.ts
@@ -11,14 +11,11 @@ import { SharedModule } from '@app/shared';
 import { MaterialModule } from '@app/material.module';
 import { ExamRouteModule } from './exam-routing.module';
 import { AceEditorModule } from 'ng2-ace-editor';
-<<<<<<< HEAD
 import { ExamReviewComponent } from './exam-review.component';
-=======
 import { SuspendedComponent } from './suspended.component';
 import { DeviceDetectorModule } from 'ngx-device-detector';
 import { CompatibilityComponent } from './compatibility.component';
 import { ThankyouComponent } from './thankyou.component';
->>>>>>> 3f3afdf4ce1ccbb468f95eed69c5cbb16afc187c
 
 
 @NgModule({
@@ -34,10 +31,6 @@ import { ThankyouComponent } from './thankyou.component';
     AceEditorModule,
     DeviceDetectorModule.forRoot()
   ],
-<<<<<<< HEAD
-  declarations: [ExamComponent, ExamReviewComponent]
-=======
-  declarations: [ExamComponent, SuspendedComponent, CompatibilityComponent, ThankyouComponent]
->>>>>>> 3f3afdf4ce1ccbb468f95eed69c5cbb16afc187c
+  declarations: [ExamComponent, SuspendedComponent, CompatibilityComponent, ThankyouComponent, ExamReviewComponent]
 })
 export class ExamModule { }
